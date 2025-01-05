84f29bed80b757d2578e4a28d90d0694df77bc4c
jd <jashansingh29@gmail.com>
2025-01-04 20:08:21 -0500

    staticfilesfix

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,18 @@
+"""-------------------------------------------------------
+Django: Core Urls.py
+-------------------------------------------------------
+Author:  JD
+ID:      91786
+Uses:    Django
+Version:  1.0.8
+__updated__ = Sat Jan 04 2025
+-------------------------------------------------------
+"""
+
 from django.urls import path
-from .views import index_view
+from core import views
+
 
 urlpatterns = [
-    path("", index_view, name="home"),
+    path("", views.index_view, name="home"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,19 @@
+"""-------------------------------------------------------
+Django: Core Views.py
+-------------------------------------------------------
+Author:  JD
+ID:      91786
+Uses:    Django
+Version:  1.0.8
+__updated__ = Sat Jan 04 2025
+-------------------------------------------------------
+"""
+
 from django.shortcuts import render, HttpResponse
+
+app_name = "core"
 
 
 # index view
 def index_view(request):
-    return render(request, "base.html")
+    return render(request, "core/index.html")
